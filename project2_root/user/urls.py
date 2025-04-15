f2aa2593737bdccdaff78f60494cc7ee6509ed47
amin.louiosman2 <amin.louiosman2@L125-58.soclabs.computing.dcu.ie>
2025-04-15 14:56:39 +0100

    User Progress 2

---
+++ project2_root/user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'), 
+    path('home/', views.HomeView.as_view(), name='home'),   
+    path('', views.index, name='index'),
+]
--- project2_root/user/views.py
+++ project2_root/user/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, FormView, RedirectView, UpdateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from .forms import SignUpForm
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+def index(request, pagename=''):
+    pagename = '/' + pagename
+    context = {
+    }
+    return render(request, 'generalhome.html', context)
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('profile')  # Or wherever you want to redirect after signup
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+    
+class CustomLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'accounts/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('home')  # Or login page or wherever
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/home.html'
+    login_url = 'login'
+
+class HomeView(View):
+    def get(self, request, pagename=''):
+        context = {
+        }
+        return render(request, 'accounts/home.html', context)
+
