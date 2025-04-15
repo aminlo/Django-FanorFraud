e8db5ec29b130ba87c200ff0f3c8b249b97e1eeb
Amin Loui Osman <amin.louiosman2@mail.dcu.ie>
2025-04-15 01:47:48 +0100

    Django initialized 2

--- project2_root/general/views.py
+++ project2_root/general/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Simple Test Site")
+def index(request, pagename=''):
+    pagename = '/' + pagename
+    context = {
+    }
+    return render(request, 'base.html', context)
